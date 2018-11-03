f587a80de23bce4696452d82565fb415994b87f1
DoomPotato <shaar@utschools.ca>
2018-11-03 11:57:55 -0400

    make stuff work

--- environmentgo/models.py
+++ environmentgo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
@@ -14,4 +14,4 @@ class Photo(models.Model):
     image = models.ImageField(upload_to='images/')
     description = models.CharField(max_length=500, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(Profile, related_name='photos')
+    uploaded_by = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='photos')
