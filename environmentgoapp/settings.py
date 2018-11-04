cb1b3e53b8e073bb3e0b13619b4194677217a950
DoomPotato <shaar@utschools.ca>
2018-11-04 00:13:05 -0400

    style login and registration templates

--- environmentgoapp/settings.py
+++ environmentgoapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y!^lr&h+puy!tfsrbdws1cnvgzmfg#c^@kxsik2few)8fw9n_h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
